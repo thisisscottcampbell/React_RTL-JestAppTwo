fc85bb444b52310145bf830af3c7b6878b90acd4
Scott Campbell <thisisscottcampbell@gmail.com>
2021-07-09 20:31:32 -0400

    test bootstrap

---
+++ app-two/src/components/summary/__test__/SummaryForm.test.jsx
@@ -0,0 +1,29 @@
+import { render, screen, fireEvent } from '@testing-library/react';
+import SummaryForm from '../SummaryForm';
+
+test('Init conditions', () => {
+	render(<SummaryForm />);
+
+	const checkbox = screen.getByRole('checkbox', {
+		name: /terms and conditions/i,
+	});
+	expect(checkbox).not.toBeChecked();
+
+	const confirmButton = screen.getByRole('button', { name: /confirm order/i });
+	expect(confirmButton).toBeDisabled();
+});
+
+test('Checkbox disables buytton on first click and enables on second', () => {
+	render(<SummaryForm />);
+
+	const checkbox = screen.getByRole('checkbox', {
+		name: /terms and conditions/i,
+	});
+	const confirmButton = screen.getByRole('button', { name: /confirm order/i });
+
+	fireEvent.click(checkbox);
+	expect(confirmButton).toBeEnabled();
+
+	fireEvent.click(checkbox);
+	expect(confirmButton).toBeDisabled();
+});
